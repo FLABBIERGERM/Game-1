5b9441bcde0248877649f3a4baf1ce64d55fb3ab
FLABBIERGERM <96157270+FLABBIERGERM@users.noreply.github.com>
2024-05-10 17:40:01 -0600

    Final changes of day
    Nephew and niece showed up and had to stop just making sure its all saved

--- scenes/main.gd
+++ scenes/main.gd
@@ -11,8 +11,6 @@ func _process(delta):
 	if Input.is_action_just_pressed("Menu"):
 		get_tree().paused = not get_tree().paused
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-
-		
 		$Pausemenu/VBoxContainer.visible = not $Pausemenu/VBoxContainer.visible 
 
 
